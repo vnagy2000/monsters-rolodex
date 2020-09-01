438516e7fc9ede28e3498c777e83591107a73ca1
unknown <vnagy2000@gmail.com>
2020-09-01 20:15:08 +0200

    Updates

---
+++ precache-manifest.115f68e2597cc56f69083a2232413931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d67b5ccbd12b67e4daa49e7c65e8c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2217ef3a98aa402437c1",
+    "url": "/monsters-rolodex/static/css/main.69c2a8c1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2217ef3a98aa402437c1",
+    "url": "/monsters-rolodex/static/js/main.612b4f4f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
